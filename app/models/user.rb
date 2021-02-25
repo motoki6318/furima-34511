877d1067c54a050290e255516cb3f1c8e6306303
motoki6318 <mksk6318@icloud.com>
2021-02-25 16:05:14 +0900

    エラーメッセージの表示を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: {message: "Email has already been taken"}
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i, meesage: "Password Include both letters and numbers"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Last name Full-width characters"}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "First name Full-width characters"}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
     validates :birthday
   end
+  # validates :email, uniqueness: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i, message: "Include both letters and numbers"}
 end
