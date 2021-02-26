9fdffe9cba38e4ffdaabd1d5cf677b934673d874
motoki6318 <mksk6318@icloud.com>
2021-02-26 15:34:56 +0900

    アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # アソシエーション
+  has_many :items
+  # ユーザー登録のバリデーション
   with_options presence: true do
     validates :nickname
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
