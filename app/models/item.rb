8271e52d0448174b20c4a6dfa2095831499fd429
motoki6318 <mksk6318@icloud.com>
2021-02-27 16:06:45 +0900

    バリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,18 @@ class Item < ApplicationRecord
   belongs_to :delivery
   has_one_attached :image
   
-  # with_options presence: true do
-    
-  # end
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
+    with_options numericality: {other_than: 0, message: 'Select'} do
+      validates :category
+      validates :condition
+      validates :postage
+      validates :prefecture
+      validates :delivery
+    end
+  end
+  validates :price, numericality: {with: /\A[0-9]+\z/, message:'Half-width number'}
 end
