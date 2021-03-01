62505c7f6a1f5aa8a0d2d70f8e2b0afa19888463
motoki6318 <mksk6318@icloud.com>
2021-03-02 08:34:24 +0900

    formの修正

--- db/migrate/20210301073215_create_shippings.rb
+++ db/migrate/20210301073215_create_shippings.rb
@@ -3,7 +3,7 @@ class CreateShippings < ActiveRecord::Migration[6.0]
     create_table :shippings do |t|
       t.references :order,          null: false, foreign_key: true
       t.string     :post_cord,      null: false
-      t.integer    :prefecture,  null: false
+      t.integer    :prefecture_id,  null: false
       t.string     :city,           null: false
       t.string     :address,        null: false
       t.string     :building_name
