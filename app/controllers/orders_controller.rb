2125d5eb89ba9f366806b38d0a402fafe8e5e90c
motoki6318 <mksk6318@icloud.com>
2021-03-02 14:33:59 +0900

    購入記録について保存の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_shipping_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_shipping_params
+    params.require(:order_shipping).permit(:post_code, :prefecture_id, :city, :address, :building_name, :tel_number)
+                  .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
