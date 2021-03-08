398fe58340ddbdeb148633f75050f2f24344bf1c
motoki6318 <mksk6318@icloud.com>
2021-03-08 16:44:05 +0900

    herokuでもActionCableを使う設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://furima-34511.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://furima-34511.herokuapp.com', 'http://furima-34511.herokuapp.com']
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
